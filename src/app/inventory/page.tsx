df2f86e420741bd45a29d9950c7a4771af76e366
kurtthezombie <kurt.desmond619@gmail.com>
2026-05-31 08:30:02 +0800

    first commit

---
+++ src/app/inventory/page.tsx
@@ -0,0 +1,3 @@
+export default function InventoryPage() {
+  return <main className="p-6">Inventory</main>;
+}
---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,3 @@
+export default function TasksPage() {
+  return <main className="p-6">Tasks</main>;
+}
